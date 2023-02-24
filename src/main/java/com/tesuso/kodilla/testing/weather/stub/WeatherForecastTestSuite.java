769f5aecb74f8f83bbb105bb6df6808544a31dfc
MaciejG78 <Maciej_G@poczta.fm>
2023-02-24 18:46:17 +0100

    WeatherForecast

---
+++ src/main/java/com/tesuso/kodilla/testing/weather/stub/WeatherForecastTestSuite.java
@@ -0,0 +1,22 @@
+package com.tesuso.kodilla.testing.weather.stub;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class WeatherForecastTestSuite {
+
+    @Test
+    void testCalculateForecastWithStub() {
+        //Given
+        Temperatures temperatures = new TemperaturesStub();                    // [1]
+        WeatherForecast weatherForecast = new WeatherForecast(temperatures);
+
+        //When
+        int quantityOfSensors = weatherForecast.calculateForecast().size();
+
+        //Then
+        Assertions.assertEquals(5, quantityOfSensors);
+    }
+
+}
