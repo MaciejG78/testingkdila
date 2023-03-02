d65af8860009fb12836f45be53cb0e43c086c9f2
MaciejG78 <Maciej_G@poczta.fm>
2023-03-02 15:42:03 +0100

    Testing

---
+++ src/main/java/FirstApp.java
@@ -0,0 +1,26 @@
+public class FirstApp {
+
+//    Class should begin with a list of variables
+    public static String PUBLIC_ATRIBUTE = "Jakaś stała nazwa";
+    protected static String PROTECTED_ATRIBUTE = "Jakaś stała nazwa";
+    private static String PRIVATE_ATRIBUTE = "Jakaś stała nazwa";
+
+    public int age;
+    protected int age2;
+    private int age3;
+
+    public FirstApp(){};
+
+    public FirstApp(int age, int age2, int age3) {
+        this.age = age;
+        this.age2 = age2;
+        this.age3 = age3;
+    }
+
+    public void publicMethod() {}
+    protected void protectedMethod() {}
+    private void privateMethod() {}
+
+    private void privateInnerMethod() {}
+
+}
---
+++ src/test/java/com/tesuso/kodilla/testing/library/BookDirectoryTestSuite.java
@@ -0,0 +1,80 @@
+package com.tesuso.kodilla.testing.library;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import testing.library.Book;
+import testing.library.BookLibrary;
+import testing.library.LibraryDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
+
+@ExtendWith(MockitoExtension.class)
+public class BookDirectoryTestSuite {
+
+    @Mock
+    private LibraryDatabase libraryDatabaseMock;
+
+
+    private List<Book> generateListOfNBooks(int booksQuantity) {
+        List<Book> resultList = new ArrayList<>();
+        for (int n = 1; n <= booksQuantity; n++) {
+            Book theBook = new Book("Title " + n, "Author " + n, 1970 + n);
+            resultList.add(theBook);
+        }
+        return resultList;
+    }
+
+    @Test
+    void testListBooksWithConditionsReturnList() {
+
+        // Given
+        BookLibrary bookLibrary = new BookLibrary(libraryDatabaseMock);
+        List<Book> resultListOf0Books = new ArrayList<>();
+        List<Book> resultListOf15Books = generateListOfNBooks(15);
+        List<Book> resultListOf40Books = generateListOfNBooks(40);
+        when(libraryDatabaseMock.listBooksWithCondition(anyString())).thenReturn(resultListOf15Books);
+        when(libraryDatabaseMock.listBooksWithCondition("ZeroBooks")).thenReturn(resultListOf0Books);
+        when(libraryDatabaseMock.listBooksWithCondition("FortyBooks")).thenReturn(resultListOf40Books);
+
+        // When
+        List<Book> theListOfBooks0 = bookLibrary.listBooksWithCondition("ZeroBooks");
+        List<Book> theListOfBooks15 = bookLibrary.listBooksWithCondition("Any title");
+        List<Book> theListOfBooks40 = bookLibrary.listBooksWithCondition("FortyBooks");
+
+        // Then
+        assertEquals(0, theListOfBooks0.size());
+        assertEquals(15, theListOfBooks15.size());
+        assertEquals(0, theListOfBooks40.size());
+    }
+
+    @Test
+    void testListBooksWithConditionMoreThan20() {
+        assertTrue(true);
+    }
+
+    @Test
+    void testListBooksWithConditionFragmentShorterThan3() {
+        // Given
+        LibraryDatabase libraryDatabaseMock = mock(LibraryDatabase.class);            // [2]
+        BookLibrary bookLibrary = new BookLibrary(libraryDatabaseMock);               // [3]
+
+        // When
+        List<Book> theListOfBooks10 = bookLibrary.listBooksWithCondition("An");       // [4]
+
+        // Then
+        assertEquals(0, theListOfBooks10.size());                                     // [5]
+        verify(libraryDatabaseMock, times(0)).listBooksWithCondition(anyString());    // [6]
+
+    }
+
+
+}
